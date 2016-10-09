11bd948d6321dfd32f1458a2754e14b0e32ad44b
GooogIe <nameless5@protonmail.com>
2016-10-09 15:24:58 +0200

    Added Chat IDs button reloader

--- Unlimitme.py
+++ Unlimitme.py
@@ -74,7 +74,10 @@ class gui(Frame):
 		frame4 = Frame(self)
 		frame4.pack(fill=X)
 
-		cidslbl = Label(frame4,text="Chat IDS found: \n"+chatids(tkntxt.get()),width=15)
+
+		x = StringVar()
+		x.set("Chat IDS found: \n"+chatids(tkntxt.get()))
+		cidslbl = Label(frame4,textvariable=x,width=35)
 		cidslbl.pack(side=LEFT,anchor=N,padx=5,pady=5)
 		
 		listbox = Listbox(frame4,width= 30)
@@ -91,6 +94,9 @@ class gui(Frame):
 		
 		savebtn = Button(frame5,text="Save CID & Token",width=12,command=lambda: self.saveall(tkntxt.get(),cidtxt.get()))
 		savebtn.pack(side=LEFT,padx=5,pady=5)
+
+		getcidsbtn = Button(frame5,text="Reload Chat IDS",width=12,command=lambda: x.set("Chat IDS found: \n"+chatids(tkntxt.get())))
+		getcidsbtn.pack(side=LEFT,padx=5,pady=5)
 		
 		frame6 = Frame(self)
 		frame6.pack(fill=X,expand=True)
@@ -141,27 +147,44 @@ class gui(Frame):
 		ftoken.close()
 
 def chatids(token):
-	url =  "https://api.telegram.org/bot" + token + "/getUpdates"
-	response = urllib2.urlopen(url).read()
-	response = json.loads(response)
-	cids = []
-	i = 0
-	while i+2< len(response['result']):
-		try:
-			id = response['result'][i]['message']['chat']['id']
-		except KeyError, e: #Thanks to @Aya - http://stackoverflow.com/questions/16154032/catch-keyerror-in-python
-    			print 'Alert - Got a KeyError - reason "%s"' % str(e)
-		if id not in cids and id <0:
-			cids.append(id)
-		else:
-			pass	
-		i +=1
-	return '\n'.join(map(str,cids))
+	if os.path.exists("token.txt") == True:
+		url =  "https://api.telegram.org/bot" + token + "/getUpdates"
+		response = urllib2.urlopen(url).read()
+		response = json.loads(response)
+		cids = []
+		i = 0
+		while i+2< len(response['result']):
+			try:
+				id = response['result'][i]['message']['chat']['id']
+				name = response['result'][i]['message']['chat']['title']
+			except KeyError, e: #Thanks to @Aya - http://stackoverflow.com/questions/16154032/catch-keyerror-in-python
+    				pass
+			id = str(id)+":"+name.encode('ascii', 'ignore')
+			
+			if id not in cids:
+				cids.append(id)
+			else:
+				pass	
+			i +=1
+		i =0
+		for ids in cids:
+			idcomp = ids.split(':',1)
+			idint = int(idcomp[0])
+			if idint>0:
+				cids.pop(i)
+			else:
+				pass
+			i +=1
+		print '\n'.join(cids)
+		return '\n'.join(cids)
+	else:
+		return "Please provide a token before."
 def main():
 	import sys
 
 	req_version = (2,10)
 	cur_version = sys.version_info
+	print cur_version
 	if cur_version < req_version:
 		root = Tk()
 		try:
@@ -171,6 +194,7 @@ def main():
 		root.resizable(height=False,width=False)
 		unlimit = gui(root)
 		root.mainloop() 
+		root.update_idletasks()
 	else:
 		print "Your Python interpreter is too new. Please consider downgrading to 2.10 or older."
 
