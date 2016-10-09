fe5cde0600a8e56d6aae82f2110995a9f2b53930
GooogIe <nameless5@protonmail.com>
2016-10-09 12:17:58 +0200

    Created Start.bat

---
+++ Start.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+IF EXIST "C:\Python24" (
+C:\Python24\python.exe Unlimitme.py
+)
+IF EXIST "C:\Python25" (
+C:\Python25\python.exe Unlimitme.py
+)
+IF EXIST "C:\Python26" (
+C:\Python26\python.exe Unlimitme.py
+)
+IF EXIST "C:\Python27" (
+C:\Python27\python.exe Unlimitme.py
+)
+IF EXIST "C:\Python28" (
+C:\Python28\python.exe Unlimitme.py
+)
+IF EXIST "C:\Python29" (
+C:\Python29\python.exe Unlimitme.py
+)
+IF EXIST "C:\Python210" (
+C:\Python210\python.exe Unlimitme.py
+)
